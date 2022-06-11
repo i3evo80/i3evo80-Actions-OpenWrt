5ed794c3a70c39041bd52550b06df3661518bdbb
i3evo80 <787928223@qq.com>
2022-06-11 15:01:40 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,8 +15,4 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-git clone https://github.com/xiaorouji/openwrt-passwall.git
-git clone https://github.com/fw876/helloworld.git
-git clone https://github.com/jerrykuku/lua-maxminddb.git
-git clone https://github.com/jerrykuku/luci-app-vssr.git  
 git clone https://github.com/vernesong/OpenClash.git  
